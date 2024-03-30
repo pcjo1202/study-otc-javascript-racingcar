c60b1bed6cfa448e16bd0930f37d54447ac82c3f
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-03-30 18:58:26 +0900

    Feat: make startRacing function
    경주를 진행하는 알고리즘을 담당하는 기능을 하는 함수들을 작성하였습니다.

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,19 @@
 import Input from './input.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
+import Output from './output.js';
 
 class RasingController {
   constructor(carInfo) {
     this.carInfo = carInfo;
+
+    // this.mockupData = {
+    //   rasingCarList: [
+    //     { carName: 'pobi', forward: 0 },
+    //     { carName: 'woni', forward: 0 },
+    //     { carName: 'jun', forward: 0 },
+    //   ],
+    //   playCount: '5',
+    // };
   }
 
   prepareRasing = async () => {
@@ -17,19 +27,55 @@ class RasingController {
     //3. 경주에 필요한 정보를 셋팅한다.
     await initRasingInfo(rasingCarList, playCount);
 
-    return console.log(this.carInfo.rasingInfo);
+    //return console.log(this.carInfo.rasingInfo);
   };
 
   startRacing = async () => {
-    const { rasingInfo } = this.carInfo;
+    const { rasingCarList, playCount } = this.carInfo.rasingInfo;
+    const { Console } = MissionUtils;
+    // const { rasingCarList, playCount } = this.mockupData;
+
     let currentPlayCount = 0;
+    const { printRoundStatus, printWinner } = new Output();
 
-    console.log(this.randomPickNumber());
+    while (playCount > currentPlayCount) {
+      //playCount 만큼 중첩된 결과를 출력
+      rasingCarList.forEach((car, index) => {
+        this.round(car);
+        //각각의 결과를 출력
+        printRoundStatus(car);
+      });
+      Console.print('\n');
 
-    // while (rasingInfo.playCount > currentPlayCount) {
-    //   pass;
-    // }
+      currentPlayCount++;
+    }
+
+    //최종 결과
+    printWinner(rasingCarList);
   };
+
+  round(car) {
+    const randomNumber = this.randomPickNumber();
+
+    if (this.isForward(randomNumber)) {
+      car.forward++;
+    }
+  }
+
+  randomPickNumber() {
+    const number = MissionUtils.Random.pickNumberInRange(0, 9);
+
+    return number;
+  }
+
+  isForward(number) {
+    //전진 여부를 파악
+    if (number >= 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 export default RasingController;
