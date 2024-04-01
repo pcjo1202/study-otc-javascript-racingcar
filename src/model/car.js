83f69c77b85f24a355959dc950d0527f4f72b349
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-04-01 11:15:16 +0900

    Refactor: car 클래스의 변수를 private 로 변경
    car 클래스를 다른 곳에서 수정하지 못하도록 private화 하고, getter setter를 올바른 방식으로 동작하도록 수정하였습니다.

--- src/model/car.js
+++ src/model/car.js
@@ -1,44 +1,48 @@
 class Car {
+  #racingInfo;
+  #racingCarList;
+  #playCount;
+
   constructor() {
     // 경주에 참여하는 자동차들과 시도 횟수! (게임을 진행하는데 필요한 정보)
-    this.racingInfo = { racingCarList: [], playCount: 0 };
+    this.#racingInfo = { racingCarList: [], playCount: 0 };
 
     // 각 자동차 상태들의 배열
     //[{carNames: [], forward: 0},{carNames: [], forward: 0}]
-    this.racingCarList = [];
+    this.#racingCarList = [];
 
     //경주 횟수
-    this.playCount = 0;
+    this.#playCount = 0;
   }
 
   //경주를 진행하는데 사용될 racingInfo를 초기화
-  initRacingInfo = async (carNames, playCount) => {
+  initRacingInfo = (carNames, playCount) => {
     carNames.forEach((list, index) => {
-      this.racingCarList[index] = { carName: list, forward: 0 };
+      this.#racingCarList[index] = { carName: list, forward: 0 };
     });
 
-    this.racingInfo.racingCarList = this.racingCarList;
-    this.racingInfo.playCount = playCount;
+    this.#racingInfo.racingCarList = this.#racingCarList;
+    this.#racingInfo.playCount = playCount;
   };
 
-  get getPlayCount() {
-    return this.playCount;
+  get playCount() {
+    return this.#playCount;
   }
 
-  get getRacingInfo() {
-    return this.racingInfo;
+  get racingInfo() {
+    return this.#racingInfo;
   }
 
-  get getRacingCarList() {
-    return this.racingCarList;
+  get racingCarList() {
+    return this.#racingCarList;
   }
 
-  set setCarName(cars) {
-    this.racingInfo.racingCarList = cars;
+  set racingCarList(cars) {
+    this.#racingCarList = cars;
   }
 
-  set setPlayCount(playCount) {
-    this.racingInfo.playCount = playCount;
+  set playCount(playCount) {
+    this.#playCount = playCount;
   }
 }
 
